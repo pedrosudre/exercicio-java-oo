107ba264a1423740df1924ada3085965de11c57a
Pedro Sudré <pedrosudre04@gmail.com>
2023-03-06 20:23:53 -0300

    construtor ator e diretor

--- src/Ator.java
+++ src/Ator.java
@@ -1,2 +1,25 @@
-public class Ator {
+public class Ator extends Pessoa{
+    private String registro;
+
+    public Ator(Diretor diretor) {
+        this.diretor = diretor;
+    }
+
+    private Diretor diretor;
+
+    public String getRegistro() {
+        return registro;
+    }
+
+    public void setRegistro(String registro) {
+        this.registro = registro;
+    }
+
+    public Diretor getDiretor() {
+        return diretor;
+    }
+
+    public void setDiretor(Diretor diretor) {
+        this.diretor = diretor;
+    }
 }
--- src/Diretor.java
+++ src/Diretor.java
@@ -1,2 +1,25 @@
-public class Diretor {
+public class Diretor extends Pessoa{
+    private Integer quantidadePremios;
+
+    public Diretor(Ator ator) {
+        this.ator = ator;
+    }
+
+    private Ator ator;
+
+    public Integer getQuantidadePremios() {
+        return quantidadePremios;
+    }
+
+    public void setQuantidadePremios(Integer quantidadePremios) {
+        this.quantidadePremios = quantidadePremios;
+    }
+
+    public Ator getAtor() {
+        return ator;
+    }
+
+    public void setAtor(Ator ator) {
+        this.ator = ator;
+    }
 }
